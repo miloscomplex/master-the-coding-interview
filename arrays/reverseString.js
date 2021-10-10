f2b081e0b25d708e2503adc0e4ab4cc70485a768
Michael Sutton <hicallmesutton@gmail.com>
2021-10-09 22:09:32 -0500

    added edge case to reverseString

--- arrays/reverseString.js
+++ arrays/reverseString.js
@@ -1,6 +1,8 @@
 // Create a function that reverses a string
 
 function reverseString(str) {
+  if (!str || str.length < 2 || typeof str !== 'string' ) return 'error';
+  
   const tempArray = []
   for (let i = str.length - 1; i >= 0; i--) {
     tempArray.push(str[i])
