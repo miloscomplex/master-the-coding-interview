cbddf7e7c4b463f1b7c06eeed894e0377f51ebb1
Michael Sutton <hicallmesutton@gmail.com>
2021-10-10 22:03:44 -0500

    Linked List with append()

---
+++ linkedList/LinkedList.js
@@ -0,0 +1,24 @@
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    }
+    this.tail = this.head
+    this.length = 1
+  }
+
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null
+    }
+    this.tail.next = newNode
+    this.tail = newNode
+    this.length++
+    return this;
+  }
+
+}
+
+let myLinkedList = new LinkedList(10)
