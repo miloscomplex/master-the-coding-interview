2b004bd276c6287e27aa9d40a791932d6c9ef02c
Michael Sutton <hicallmesutton@gmail.com>
2021-10-10 22:49:40 -0500

    added printList() and insert() methods

--- linkedList/LinkedList.js
+++ linkedList/LinkedList.js
@@ -1,3 +1,10 @@
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -9,10 +16,7 @@ class LinkedList {
   }
 
   append(value) {
-    const newNode = {
-      value: value,
-      next: null
-    }
+    const newNode = new Node(value)
     this.tail.next = newNode
     this.tail = newNode
     this.length++
@@ -20,15 +24,49 @@ class LinkedList {
   }
 
   prepend(value) {
-    const newNode = {
-      value: value,
-      next: this.head
-    }
+    const newNode = new Node(value)
+    newNode.next = this.head
     this.head = newNode
     this.length++
     return this
   }
 
+  printList() {
+    const array = []
+    let currentNode = this.head
+    while (currentNode !== null) {
+      array.push(currentNode.value)
+      currentNode = currentNode.next
+    }
+    return array
+  }
+
+  insert(index, value) {
+    if (index === 0) {
+      this.prepend(value)
+      return this.printList()
+    }
+    const newNode = new Node(value)
+    let counter = 0
+    let currentNode = this.head;
+    let prevNode;
+    while (counter < index) {
+      prevNode = currentNode
+      currentNode = currentNode.next
+      counter++
+    }
+    prevNode.next = newNode
+    newNode.next = currentNode
+    length++
+    return this.printList()
+  }
+
+
+
 }
 
 let myLinkedList = new LinkedList(10)
+
+myLinkedList.append(12)
+myLinkedList.append(34)
+myLinkedList.append(56)
