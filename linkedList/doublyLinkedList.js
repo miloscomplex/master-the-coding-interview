83146f38a5104eccbc16be1d309d3535e4e94b7e
Michael Sutton <hicallmesutton@gmail.com>
2021-10-11 22:27:59 -0500

    progress commit for DoublyLinkedList()

--- linkedList/doublyLinkedList.js
+++ linkedList/doublyLinkedList.js
@@ -22,8 +22,56 @@ class DoublyLinkedList {
     this.tail.next = newNode
     this.tail = newNode
     this.length++
-    return this;
+    return this
   }
+
+  prepend(value) {
+    const newNode = new Node(value)
+    newNode.next = this.head
+    this.head.prev = newNode
+    this.head = newNode
+    this.length++
+    return this
+  }
+
+  printList() {
+    const array = []
+    let currentNode = this.head
+    while (currentNode !== null) {
+      array.push(currentNode.value)
+      currentNode = currentNode.next
+    }
+    return array
+  }
+
+  traverseToIndex(index) {
+    let counter = 0
+    let currentNode = this.head
+    while (counter !== index) {
+      currentNode = currentNode.next
+      counter++
+    }
+    return currentNode
+  }
+
+  insert(index, value) {
+    if (index === 0) return this.prepend(value);
+    if (index >= this.length) return this.append(value);
+
+    const newNode = new Node(value)
+    const leader = this.traverseToIndex(index-1)
+    const follower = leader.next
+    leader.next = newNode
+    newNode.prev = leader
+    newNode.next = follower
+    follower.prev = newNode
+    length++
+    return this.printList()
+  }
+
 }
 
 const newLinkedList = new DoublyLinkedList(10)
+newLinkedList.append(12)
+newLinkedList.append(34)
+newLinkedList.prepend(56)
