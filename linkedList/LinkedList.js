cd23c40af11a4614eb2c8c6064c4dc555359fd50
Michael Sutton <hicallmesutton@gmail.com>
2021-10-11 23:36:16 -0500

    added reverse() to linkedList Class

--- linkedList/LinkedList.js
+++ linkedList/LinkedList.js
@@ -73,6 +73,22 @@ class LinkedList {
     return this.printList()
   }
 
+  reverse() {
+      if (!this.head.next) return this;
+      let first = this.head
+      this.tail = this.head
+      let second = first.next
+      while (second) {
+        const temp = second.next
+        second.next = first
+        first = second
+        second = temp
+      }
+      this.head.next = null
+      this.head = first
+      return this.printList()
+  }
+
 }
 
 let myLinkedList = new LinkedList(10)
