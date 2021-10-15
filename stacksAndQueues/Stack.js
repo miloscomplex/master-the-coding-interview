c5ea9f27c289d0821b6a9d399524b55ba600d090
Michael Sutton <hicallmesutton@gmail.com>
2021-10-14 23:52:10 -0500

    Created Stack.js with peek() push() pop() and isEmpty()

--- stacksAndQueues/Stack.js
+++ stacksAndQueues/Stack.js
@@ -11,20 +11,55 @@ class Stack {
     this.bottom = null;
     this.length = 0;
   }
+
   peek() {
+    // see top node
+    if (!this.isEmpty()) {
+      return this.top
+    } else {
+      return null
+    }
   }
-  push(value){
+
+  push(value) {
+    const newNode = new Node(value)
+    if (this.top) {
+      newNode.next = this.top
+    } else {
+      this.top = newNode
+      this.bottom = this.top
+    }
+    this.top = newNode
+    this.length++
+    return this
   }
-  pop(){
+
+  pop() {
+    if (!this.top) return null;
+    const tempNode = this.top
+    if (tempNode.next === null) {
+      this.bottom = null
+      this.top = null
+    } else {
+      this.top = tempNode.next
+    }
+    this.length--
+    return tempNode
   }
-  //isEmpty
+
+  isEmpty() {
+    if (this.length === 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+
 }
 
 const myStack = new Stack();
 
 
-
-
-//Discord
-//Udemy
-//google
+myStack.push('Discord')
+myStack.push('Udemy')
+myStack.push('google')
